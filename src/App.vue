fefce3928708e7db06eb3a1ee8c2e9e952fe95e2
Nitro888 <nitro88x@gmail.com>
2019-03-14 15:23:20 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app dark>
+    <v-toolbar app flat>
+      <v-toolbar-title class="headline text-uppercase">
+        DaoAuth
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        icon
+        href="https://github.com/Nitro888/DaoAuth"
+        target="_blank"
+      >
+        <v-icon>fab fa-github</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <Home/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Home from './views/Home'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-container>
+    <h1>This is a home page</h1>
+  </v-container>
+</template>
